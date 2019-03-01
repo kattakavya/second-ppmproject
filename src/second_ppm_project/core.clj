a9040f2fea8d8b7e34f13a8cf597daddfefac04b
kattakavya <kavya@front.live>
2019-03-01 09:06:15 +0530

    changes

---
+++ src/second_ppm_project/core.clj
@@ -0,0 +1,19 @@
+(ns second-ppm-project.core)
+
+(def  head (str "P3\n" 400" " 600 "\n255\n"))
+
+(def foo
+(let [
+      
+      matrix   (clojure.string/join (for [j (range  600) 
+                                          i (range 0 400)
+                                          :let  [r (mod i 255)
+                                                 g (mod j 255)
+                                                 b (/(* i j)255)
+                                                 ]
+                                          ]
+                                      (str r" " g" " b)))]
+  (spit "test2.ppm" head )
+  (spit "test2.ppm" matrix)))
+
+
---
+++ test/second_ppm_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns second-ppm-project.core-test
+  (:require [clojure.test :refer :all]
+            [second-ppm-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
