b3f0ffafc250a9f02db95f6ceb3426318d6df2a5
kattakavya <kavya@front.live>
2019-03-01 22:00:12 +0530

    changes

--- src/second_ppm_project/core.clj
+++ src/second_ppm_project/core.clj
@@ -2,16 +2,20 @@
 
 
 
-(def head (str "P3\n" 400" " 600 "\n255\n"))
+(def head (str "P3\n" 600 " " 400 " 255 " "\n"))
 
-(def foo             (for [j (range  600)
-                              i (range 0 400)
-                                :let  [r (mod i 255)
-                                       g (mod j 255)
-                                      b (/(* i j)255)
-                                          
-                 (str head (clojure.string/join(r" " g" " b"\n")))]])
-(spit "test2.ppm" head)
-  (spit "test2.ppm" foo))
+(def foo
+  (for [j (range 600)
+        i (range 400)
+        :let
+        [r (mod i 255)
+         g (mod j 255)
+         b (mod (* i j)255)]
+        ]
+    (clojure.string/join "flush-on-newline"  [r " " g " " b "\n"])))
 
+(def result
+  (apply str head foo))
+
+(spit "test2.ppm" result :append true)
 
